d5a97b49bcdcaeac9145863f5e3ab3746cb8fff1
Wei Guangcheng <twksos@qq.com>
2018-09-23 05:04:23 +0100

    eosjs2

--- app/App.js
+++ app/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
+import {Platform, StatusBar, StyleSheet, View} from 'react-native';
+import {AppLoading, Asset, Font, Icon} from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import eosjs2 from 'eosjs2';
+const defaultPrivateKey = "5JtUScZK2XEp3g9gh7F8bwtPTRAkASmNrrftmx4AxDKD5K4zDnr"; // useraaaaaaaa
+const rpc = new eosjs2.Rpc.JsonRpc('http://127.0.0.1:8000');
+const signatureProvider = new eosjs2.SignatureProvider([defaultPrivateKey]);
+const api = new eosjs2.Api({ rpc, signatureProvider});
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
+  async componentDidMount() {
+    const result = await api.transact({
+      actions: [{
+        account: 'eosio.token',
+        name: 'transfer',
+        authorization: [{
+          actor: 'useraaaaaaaa',
+          permission: 'active',
+        }],
+        data: {
+          from: 'useraaaaaaaa',
+          to: 'useraaaaaaab',
+          quantity: '0.0001 SYS',
+          memo: '',
+        },
+      }]
+    }, {
+      blocksBehind: 3,
+      expireSeconds: 30,
+    });
+    console.log(result);
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
@@ -20,8 +47,8 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          {Platform.OS === 'ios' && <StatusBar barStyle="default"/>}
+          <AppNavigator/>
         </View>
       );
     }
@@ -50,7 +77,7 @@ export default class App extends React.Component {
   };
 
   _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
+    this.setState({isLoadingComplete: true});
   };
 }
 
