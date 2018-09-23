a78108d58c28a5a293ae70c4c26ac55d4a667398
Wei Guangcheng <twksos@qq.com>
2018-09-23 11:52:22 +0100

    fix scan

--- eosjs-react-native/screens/OrganizerScanScreen.js
+++ eosjs-react-native/screens/OrganizerScanScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
+import {EOSService} from '../services/EOSService';
+import {sha256} from 'js-sha256';
 
 const styles = StyleSheet.create({
   container: {
@@ -36,10 +38,21 @@ export default class OrganizerScanScreen extends React.Component {
     header: null
   };
 
+  async check() {
+    const {
+      navigation: {navigate}
+    } = this.props;
+    const name = 'TicketHash';
+    const passport = 'G12345678';
+    const hash = sha256(name + ',' + passport);
+    await EOSService.checkTicket({hash, seller: 'ticketsella1'});
+    navigate("OrganizerVerified");
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.fullScreen} onPress={()=>this.props.navigation.navigate("OrganizerVerified")}>
+        <TouchableOpacity style={styles.fullScreen} onPress={() => this.check()}>
           <Image
             source={require("../assets/images/scan.png")}
             style={styles.welcomeImage}
