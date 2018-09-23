d1cfd846c089e29cbcef60ef33548e3cbd52125d
Wei Guangcheng <twksos@qq.com>
2018-09-23 11:23:43 +0100

    add assets

---
+++ eosjs-react-native/constants/Assets.js
@@ -0,0 +1,13 @@
+export const Asset = {
+  confirm: require('./assets/images/confirmation.png'),
+  event1: require('./assets/images/event_1.png'),
+  event2: require('./assets/images/event_2.png'),
+  event3: require('./assets/images/event_3.png'),
+  eventHeader: require('./assets/images/event_details_header.png'),
+  G3: require('./assets/images/G3.png'),
+  G2: require('./assets/images/G2.png'),
+  G1: require('./assets/images/G1.png'),
+  G0: require('./assets/images/G0.png'),
+  homeHeader: require('./assets/images/homeHeader.png'),
+  map: require('./assets/images/map.png'),
+}
